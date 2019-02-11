e4439457925b78bda07bc086ba15e872d6bcf364
Ernesto Suárez Ramírez <ernestosu16@gmail.com>
2019-02-11 01:01:34 -0500

    Agregando mas cambios y el admin del paciente

---
+++ src/Entity/AppPaciente.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class AppPaciente
+ * @package App\Entity
+ *
+ * @ORM\Entity()
+ * @ORM\HasLifecycleCallbacks()
+ */
+class AppPaciente extends _BaseEntity_
+{
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=15, unique=true)
+     */
+    protected $ci;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=150)
+     */
+    protected $nombre;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=15)
+     */
+    protected $telefono_contacto;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=50)
+     */
+    protected $correo_contacto;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=150)
+     */
+    protected $direccion;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=15)
+     */
+    protected $historia_clinica;
+
+    public function getCi(): ?string
+    {
+        return $this->ci;
+    }
+
+    public function setCi(string $ci): self
+    {
+        $this->ci = $ci;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getTelefonoContacto(): ?string
+    {
+        return $this->telefono_contacto;
+    }
+
+    public function setTelefonoContacto(string $telefono_contacto): self
+    {
+        $this->telefono_contacto = $telefono_contacto;
+
+        return $this;
+    }
+
+    public function getCorreoContacto(): ?string
+    {
+        return $this->correo_contacto;
+    }
+
+    public function setCorreoContacto(string $correo_contacto): self
+    {
+        $this->correo_contacto = $correo_contacto;
+
+        return $this;
+    }
+
+    public function getDireccion(): ?string
+    {
+        return $this->direccion;
+    }
+
+    public function setDireccion(string $direccion): self
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    public function getHistoriaClinica(): ?string
+    {
+        return $this->historia_clinica;
+    }
+
+    public function setHistoriaClinica(string $historia_clinica): self
+    {
+        $this->historia_clinica = $historia_clinica;
+
+        return $this;
+    }
+
+}
--- src/EventListener/MenuBuilderListener.php
+++ src/EventListener/MenuBuilderListener.php
@@ -4,14 +4,26 @@ namespace App\EventListener;
 
 
 use Knp\Menu\ItemInterface;
-use Knp\Menu\MenuItem;
 use Sonata\AdminBundle\Event\ConfigureMenuEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class MenuBuilderListener
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function addMenuItems(ConfigureMenuEvent $event)
     {
         $menu = $event->getMenu();
+
+        $roles = array();
+        if($listRoles = $this->container->get('security.token_storage')->getToken()){
+            $roles = $listRoles->getRoles();
+        }
 
         $listMenu = $menu->getChildren();
 
@@ -23,12 +35,17 @@ class MenuBuilderListener
             ->setExtras(['icon' => '<i class="fa fa-angle-double-right"></i>']);
         $administrationMenu = $this->insertValueAtPosition($administrationMenu, $childChangePassword, 1, "app.change_password");
 
-        $childBackups = $event->getFactory()->createItem('sonata.user.admin.backup', [
-            'label' => 'app.backup',
-            'route' => 'admin_backups'])
-            ->setExtras(['icon' => '<i class="fa fa-angle-double-right"></i>']);
+        foreach ($roles as $role) {
+            if($role->getRole() === 'ROLE_ESPECIALISTA_SISTEMA' || $role->getRole() === 'ROLE_SUPER_ADMIN'){
+                $childBackups = $event->getFactory()->createItem('sonata.user.admin.backup', [
+                    'label' => 'app.backup',
+                    'route' => 'admin_backups'])
+                    ->setExtras(['icon' => '<i class="fa fa-angle-double-right"></i>']);
 
-        $administrationMenu = $this->insertValueAtPosition($administrationMenu, $childBackups, 10, "app.backups_database");
+                $administrationMenu = $this->insertValueAtPosition($administrationMenu, $childBackups, 10, "app.backups_database");
+            }
+        }
+
 
         $listMenu['sonata.admin.group.administration']
             ->setChildren($administrationMenu);
