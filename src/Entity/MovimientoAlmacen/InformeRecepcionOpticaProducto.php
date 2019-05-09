59d1b21a496ae7797a552fb2758b001093978f4e
Ernesto Suárez Ramírez <ernestosu16@gmail.com>
2019-05-09 00:29:13 -0400

    fecha de cracion

--- src/Entity/MovimientoAlmacen/InformeRecepcionOpticaProducto.php
+++ src/Entity/MovimientoAlmacen/InformeRecepcionOpticaProducto.php
@@ -6,9 +6,15 @@ namespace App\Entity\MovimientoAlmacen;
 
 use App\Entity\_BaseEntity_;
 use App\Entity\AppProducto;
+use DateTime;
 
 class InformeRecepcionOpticaProducto extends _BaseEntity_
 {
+    public function __construct()
+    {
+        $this->created_at = new DateTime();
+    }
+
     public function __toString()
     {
         /** @var AppProducto $producto */
